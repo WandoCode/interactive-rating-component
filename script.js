4881f9de02f020afa238282e653d97b0103868c7
Wandole <58407717+Wandole@users.noreply.github.com>
2022-05-23 11:56:27 +0300

    Changed buttons to radio input for rating value choice

--- script.js
+++ script.js
@@ -1,37 +1,16 @@
-const ratingEl = document.getElementsByClassName('rating')[0];
 const thanksEl = document.getElementsByClassName('thanks')[0];
 const submitBtn = document.getElementsByClassName("rating__submit")[0];
 const displayEl = document.getElementsByClassName("thanks__display__result")[0]
-const rateBtns = document.getElementsByClassName("rating__score__btn");
-let ratingValue = null;
+const ratingForm = document.getElementsByClassName('rating')[0];
 
-for (let i = 0; i < rateBtns.length; i++) {
-    rateBtns[i].addEventListener('click', e => {
-        e.preventDefault();
-        hightLightBtn(i);
-        ratingValue = e.target.innerText;
-        displayValue();
-    });
-}
 
 submitBtn.addEventListener('click', e => {
     e.preventDefault();
+    const ratingValue = ratingForm["rating__score__btn"].value;
     if (ratingValue) {
-        ratingEl.style.display = "none";
+        displayEl.innerText = ratingValue;
+        ratingForm.style.display = "none";
         thanksEl.style.display = "flex";
     }
 })
 
-const hightLightBtn = (position) => {
-    for (let i = 0; i < rateBtns.length; i++) {
-        if (i === position) {
-            rateBtns[i].classList.add("active");
-        } else {
-            rateBtns[i].classList.remove("active");
-        }
-    }
-}
-
-const displayValue = () => {
-    displayEl.innerText = ratingValue;
-}
