0249536f119b22bcec71609cdd0cb2d1bf8049bf
Wandole <58407717+Wandole@users.noreply.github.com>
2022-05-23 15:33:35 +0300

    Add flip animation on form validation. Add other animation on submit btn and radio btns

--- script.js
+++ script.js
@@ -2,15 +2,19 @@ const thanksEl = document.getElementsByClassName('thanks')[0];
 const submitBtn = document.getElementsByClassName("rating__submit")[0];
 const displayEl = document.getElementsByClassName("thanks__display__result")[0]
 const ratingForm = document.getElementsByClassName('rating')[0];
-
+const mainEl = document.getElementsByClassName('comp-rating')[0];
 
 submitBtn.addEventListener('click', e => {
     e.preventDefault();
     const ratingValue = ratingForm["rating__score__btn"].value;
     if (ratingValue) {
         displayEl.innerText = ratingValue;
-        ratingForm.style.display = "none";
-        thanksEl.style.display = "flex";
+        mainEl.classList.add("slideIn");
+        mainEl.addEventListener('animationend', e => {
+            ratingForm.style.display = "none";
+            thanksEl.style.display = "flex";
+            mainEl.classList.add("slideOut")
+        })
     }
 })
 
